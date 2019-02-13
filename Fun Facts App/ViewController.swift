4bdda113a20b0eff9894f6797d993b8fb15f551c
Alice Muir <alicemuir@alices-mbp>
2019-02-13 14:04:36 +0100

    Initial Commit

---
+++ Fun Facts App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fun Facts App
+//
+//  Created by Alice Muir on 13.02.19.
+//  Copyright © 2019 Alice Muir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Fun Facts AppTests/Fun_Facts_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Fun_Facts_AppTests.swift
+//  Fun Facts AppTests
+//
+//  Created by Alice Muir on 13.02.19.
+//  Copyright © 2019 Alice Muir. All rights reserved.
+//
+
+import XCTest
+@testable import Fun_Facts_App
+
+class Fun_Facts_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
